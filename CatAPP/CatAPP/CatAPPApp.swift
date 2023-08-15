0b81b503c26eac013afec03b00aa352239149d0e
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-08-15 08:40:30 -0700

    Created Project

---
+++ CatAPP/CatAPP/CatAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPPApp.swift
+//  CatAPP
+//
+//  Created by Moises Lopez on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CatAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
