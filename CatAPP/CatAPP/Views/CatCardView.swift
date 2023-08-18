33d91659c2c7fe99db97d29b6bdd8118a59aed1c
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-08-18 08:30:52 -0700

    CreatedCatCardView

---
+++ CatAPP/CatAPP/Views/CatCardView.swift
@@ -0,0 +1,20 @@
+//
+//  CatCardView.swift
+//  CatAPP
+//
+//  Created by Moises Lopez on 18/08/23.
+//
+
+import SwiftUI
+
+struct CatCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatCardView()
+    }
+}
