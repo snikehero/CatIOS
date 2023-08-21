3c7886c4e15057667a0756bdeaf88dadd453e76c
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-08-21 08:23:05 -0700

    ChangedMockName

--- CatAPP/CatAPP/Models/PetModel.swift
+++ CatAPP/CatAPP/Models/PetModel.swift
@@ -15,7 +15,7 @@ struct PetDetail {
 }
 
 extension PetDetail {
-    static var mock = PetDetail(name: "Pitt", petYear: 7, breed: "Cruza", appointment: Date.now)
+    static var mock = PetDetail(name: "Sushi", petYear: 7, breed: "Cruza", appointment: Date.now)
     static var mock1 = PetDetail(name: "Jojo", petYear: 4, breed: "Electrico", appointment: Date.now)
     static var mock2 = PetDetail(name: "Momo", petYear: 3, breed: "Volador", appointment: Date.now)
     static var mock3 = PetDetail(name: "Mica", petYear: 5, breed: "Callejero", appointment: Date.now)
