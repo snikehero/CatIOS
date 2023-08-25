cfcbb71ce916d9aaaa819aad9cc107041c134476
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-08-25 08:19:31 -0700

    ShowCatDetailsView similar to CatDetails

--- CatAPP/CatAPP/Views/ShowCatDetailsView.swift
+++ CatAPP/CatAPP/Views/ShowCatDetailsView.swift
@@ -10,21 +10,54 @@ import SwiftUI
 struct ShowCatDetailsView: View {
     let singlePet: PetDetail
     fileprivate typealias DetailsConstants = Constants.Details
+    @Environment(\.dismiss) var dismiss
+    @State private var showingAlert = false
+    @State var petName: String = ""
+    @State var petAge: Int = 0
+    @State var petAppointment: Date = Date.now
+    @State var petBreed: String = ""
     var body: some View {
-        Form {
-            HStack {
-                Spacer()
-                PhotoPicker()
-                Spacer()
+        NavigationStack {
+            Form {
+                HStack {
+                    Spacer()
+                    PhotoPicker()
+                    Spacer()
+                }
+                Section(header: Text(DetailsConstants.detailsSection)) {
+                    TextField(DetailsConstants.nameForm,
+                              text: $petName)
+                    Picker(DetailsConstants.ageForm, selection: $petAge) {
+                        ForEach(0 ..< 20) {
+                            Text("\($0) \(DetailsConstants.ageLabel)")
+                        }
+                    }
+                    TextField(DetailsConstants.breedForm,
+                              text: $petBreed)
+                    DatePicker(selection: $petAppointment, in: Date.now...,
+                               displayedComponents: .date) {
+                        Text(DetailsConstants.dateForm)
+                    }
+                }
+                Section(header: Text(DetailsConstants.vaccineSection)) {
+                    VaccineFormView()
+                }
             }
-            Section(header: Text(DetailsConstants.detailsSection)) {
-                Text(singlePet.name)
-                Text(singlePet.breed)
-                Text(singlePet.petYear.description)
-                Text(singlePet.appointment.description)
-            }
-            Section(header: Text(DetailsConstants.vaccineSection)) {
-                VaccineFormView()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAlert = true
+                    } label: {
+                        Text("Edit")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text(DetailsConstants.cancelButton)
+                    }
+                }
             }
         }
     }
