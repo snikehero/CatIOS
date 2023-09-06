e3a0baf2e330094ffc776d42fb6231ee6205ff0a
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2023-09-06 10:50:42 -0700

    Fix the CatTag to receive Data from API

--- CatAPP/CatAPP/Models/CatTagModel.swift
+++ CatAPP/CatAPP/Models/CatTagModel.swift
@@ -7,15 +7,4 @@
 
 import Foundation
 
-struct CatTagModel: Codable {
-    let name: Int
-    let value: String
-    enum CodingKeys: String, CodingKey {
-        case name = "Name"
-        case value = "Value"
-    }
-}
-extension CatTagModel {
-    static let mock = CatTagModel(name: 0, value: "Tabby")
-
-}
+typealias CatTagModel = String
